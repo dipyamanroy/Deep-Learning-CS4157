76f1a8c5c987d6c901370a6a6d179f3decfb995c
dipyamanroy <d1pyamanroy@protonmail.com>
2024-09-29 19:41:34 +0530

    Made changes to CCPP config.

--- assignment-1/ccpd/ccpp_config.py
+++ assignment-1/ccpd/ccpp_config.py
@@ -1,14 +1,14 @@
 # Configuration for ANN
 
 # Set hyperparameters
-mini_batch_size = 256
+mini_batch_size = 64
 learning_rate = 0.001
 num_iterations = 500
 activation = "tanh"
 cost_func = "mse"
 
 # Regularization
-regularisation = "none"  # none or L2
+regularisation = "L2"  # none or L2
 lambd = 0.1
 
 # Optimizer
@@ -19,10 +19,10 @@ beta2 = 0.999
 epsilon = 1e-8
 
 # Define model
-layers_dims = [4, 10, 10, 1]
+layers_dims = [4, 10, 10, 10, 1]
 
 # Dropout
-dropout_keep_prob = 0.8  # Set to 1.0 to disable dropout
+dropout_keep_prob = 1.0  # Set to 1.0 to disable dropout
 
 # Early stopping patience (optional, None for no early stopping)
-early_stopping_patience = 20  # Stop if no improvement in validation error after 20 epochs (set to None to disable)
+early_stopping_patience = 0 # Stop if no improvement in validation error after 20 epochs (set to 0 to disable)
